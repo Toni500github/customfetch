47ba85421a18d87df4c3bea1ae2608ae8eccc01c
Toni500git <toni500minecraft@gmail.com>
2024-06-30 21:54:18 +0200

    ugh

--- include/platform.hpp
+++ include/platform.hpp
@@ -1,24 +1,27 @@
 #ifndef PLATFORM_H
 #define PLATFORM_H
 
+#include <cstddef>
+#include <cstdlib>
+
 #if (defined(unix) || defined(__unix) || defined(__unix__))
 # define CF_UNIX 1
 #endif
 
-#if (defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__))
+#if !(defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__))
 #define CF_WINDOWS 1
+typedef unsigned short int u_short; // they don't have sys/types.h XDDD
 // https://github.com/msys2/MINGW-packages/issues/4999#issuecomment-1530791650
 char *strndup(const char* src, size_t size) {
   size_t len = strnlen(src, size);
   len = len < size ? len : size;
-  char * dst = malloc(len + 1);
+  char *dst = (char*)malloc(len + 1);
   if (!dst)
-    return NULL;
+    return nullptr;
   memcpy(dst, src, len);
   dst[len] = '\0';
-  return result;
+  return dst;
 }
-typedef unsigned short int u_short; // they don't have sys/types.h XDDD
 #endif
 
 #endif // PLATFORM_H
