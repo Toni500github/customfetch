a9fd254962c626728106ccf6f07d9b56ba2913ec
Toni500git <toni500minecraft@gmail.com>
2024-12-08 17:59:34 +0100

    android app: truncate text if too large (HELL YEAH)
    ok so we finally found how to truncate text based on the widget width. Now it works out-of-box with android_small.txt, but with others doesn't really well, but it's a thing now. merging soon to main

--- android/app/src/main/java/org/toni/customfetch_android/widget/customfetch.kt
+++ android/app/src/main/java/org/toni/customfetch_android/widget/customfetch.kt
@@ -3,10 +3,17 @@ package org.toni.customfetch_android.widget
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.os.Bundle
+import android.text.SpannableStringBuilder
 import android.text.Spanned
+import android.text.TextPaint
+import android.text.TextUtils.TruncateAt
+import android.text.TextUtils.ellipsize
+import android.util.TypedValue
 import android.widget.RemoteViews
 import androidx.core.text.HtmlCompat
 import org.toni.customfetch_android.R
+
 
 /**
  * Implementation of App Widget functionality.
@@ -31,6 +38,43 @@ class customfetch : AppWidgetProvider() {
         }
     }
 
+    override fun onAppWidgetOptionsChanged(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetId: Int,
+        newOptions: Bundle
+    ) {
+        // Get the new widget size
+        val minWidthDp = newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH)
+        val maxWidthDp = newOptions.getInt(AppWidgetManager.OPTION_APPWIDGET_MAX_WIDTH)
+        val minWidthPx = convertDpToPx(context, minWidthDp)
+        val maxWidthPx = convertDpToPx(context, maxWidthDp)
+
+        val parsedContent = SpannableStringBuilder()
+        val arguments = loadTitlePref(context, appWidgetId)
+        val htmlContent = customfetchConfigureActivity().mainAndroid("customfetch $arguments")
+
+        val eachLine = htmlContent!!.split("<br>").map { it.trim() }
+        val paint = TextPaint()
+        for (line in eachLine) {
+            var parsedLine = HtmlCompat.fromHtml(line, HtmlCompat.FROM_HTML_MODE_LEGACY)
+            parsedLine = ellipsize(parsedLine, paint, minWidthDp * 0.8f, TruncateAt.END) as Spanned
+            parsedContent.appendLine(parsedLine)
+        }
+
+        val views = RemoteViews(context.packageName, R.layout.customfetch)
+        views.setTextViewText(R.id.customfetch_text, parsedContent)
+        appWidgetManager.updateAppWidget(appWidgetId, views)
+    }
+
+    private fun convertDpToPx(context: Context, dp: Int): Int {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp.toFloat(),
+            context.resources.displayMetrics
+        ).toInt()
+    }
+
     override fun onEnabled(context: Context) {
         // Enter relevant functionality for when the first widget is created
     }
@@ -46,14 +90,9 @@ internal fun updateAppWidget(
     appWidgetId: Int,
     initConfigureActivity: Boolean = false
 ) {
-    val arguments = loadTitlePref(context, appWidgetId)
-    val htmlContent =
-        if (initConfigureActivity) customfetchConfigureActivity().mainAndroid("customfetch $arguments")
-        else "useless text"
-
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.customfetch)
-    views.setTextViewText(R.id.customfetch_text, htmlContent?.let { HtmlCompat.fromHtml(it, HtmlCompat.FROM_HTML_MODE_LEGACY) })
+    views.setTextViewText(R.id.customfetch_text, "Loading...")
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
